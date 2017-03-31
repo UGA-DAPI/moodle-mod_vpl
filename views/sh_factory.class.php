250f2d99b1ab303205f3439db1204ba44d79268a
fabmen <menard.fabrice@gmail.com>
2017-03-31 16:46:48 +0200

    change sh_factory

--- views/sh_factory.class.php
+++ views/sh_factory.class.php
@@ -69,6 +69,8 @@ class vpl_sh_factory {
                 return self::get_object( 'prolog' );
             case 'm' :
                 return self::get_object( 'matlab' );
+            case 'mod' :
+                return self::get_object( 'opl' );
             case 'py' :
                 return self::get_object( 'python' );
             case 'scala' :
